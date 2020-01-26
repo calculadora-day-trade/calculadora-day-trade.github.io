6fd0ff7c0a837fd7649e94f00e36c3cf06e95a3c
Thales Marega <thales.bm92@gmail.com>
2020-01-26 09:52:07 -0300

    added validate

--- js/app.js
+++ js/app.js
@@ -3,12 +3,47 @@
  * @param input Files
  */
 function readFile(input) {
-	for (i = 0; i < input.files.length; i++) {
-		readPDF(input.files[i], function(text) {
-			var model = [analyse(text)]
-			print(model)
-		});
+
+	var isValid = validadeInput(input.files)
+
+	if (isValid != "") {
+		$("#errorAlert").show()
+		document.getElementById("errorAlert").innerHTML = isValid
+
+	} else {
+
+		$("#errorAlert").hide()
+		for (i = 0; i < input.files.length; i++) {
+			readPDF(input.files[i], function(text) {
+				var model = [analyse(text)]
+				print(model)
+			});
+		}
 	}
+}
+
+/**
+ * Metodo responsavel em validar o anexo
+ * @param input Files
+ */
+function validadeInput(files) {
+	var isValid = ""
+
+	if (files.length > 30) {
+		isValid = "Número máximo de arquivos suportados."
+
+	} else {
+		for (i = 0; i < files.length; i++) {
+			if (files[i].type != "application/pdf") {
+				isValid = "Formato de arquivo incorreto."
+				break
+
+			} else if ((files[i].size / 1000) > 100) {
+				isValid = "Tamanho do arquivo maior que o máximo permitido."
+			}
+		}
+	}
+	return isValid
 }
 
 /**
@@ -17,9 +52,14 @@ function readFile(input) {
  */
 function print(model) {
 	$("#resultTable").show()
+	$("#resultDescription").show()
 
  	$.each(model, function (index, m) {
-        var nTr = "<tr>"
+ 		var nTr = "<tr style='background-color: #F4D6D5' !important>"
+ 		if (m.gain) {
+ 			nTr = "<tr style='background-color: #D5F4E6' !important>"
+ 		}
+        
         nTr += "<td>" + m.date + "</td>"
         nTr += "<td>" + m.noteNumber + "</td>"
         nTr += "<td>" + m.grossValue + "</td>"
@@ -33,21 +73,10 @@ function print(model) {
 }
 
 /**
- * Metodo que exibe um dialog com uma mensagem de erro
- * @param text String
- */
-function showDialog(text) {
-	var dialog = $('#validateModal')
-	dialog.find('.modal-body p').text(text)
-	dialog.modal('show')
-}
-
-/**
  * Metodo que esconde varios componentes quando carrega a tela
  */
 function onloadScreen() {
-	// $("#successAlert").hide()
 	$("#errorAlert").hide()
-	// $(".progress").hide()
+	$("#resultDescription").hide()
 	$("#resultTable").hide()
 }
